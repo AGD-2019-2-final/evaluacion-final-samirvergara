1683402e6329f114384c7fcf494b017e2fcfbe43
samirvergara <sfvergar@unal.edu.co>
2019-12-24 08:02:50 -0500

    v2 20191224

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -1,4 +1,20 @@
 import sys
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+
+if __name__ == '__main__':
+
+    n=0
+    printed = 0
+    minimo = 999
+
+    while n < 6: 
+
+        for line in sys.stdin:
+            key, val = line.split("\t")
+            val = int(val)
+
+            if printed < val < minimo:
+                minimo = val
+                minkey = key
+        sys.stdout.write("{},{}\n".format(minkey, minimo))     
+        printed = printed + 1
+        n = n + 1
--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -1,4 +1,27 @@
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+
+if __name__ == '__main__':
+
+    curkey = None
+    maximo = 0.0
+    promedio = 0.0
+    n=1
+
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+            maximo = maximo + val
+            promedio = maximo / n
+                        
+        else:
+            
+            if curkey is not None:                
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo, promedio))
+            n=1
+            curkey = key
+            maximo = val
+            promedio = maximo / n
+        n=n+1
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, maximo, promedio))
